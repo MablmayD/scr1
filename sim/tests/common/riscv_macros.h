02585c2e111c097f786d69136fdf72bf66b80a3b
MablmayD <samoylovvlad1337@gmail.com>
2020-04-22 23:06:21 +0300

    goodjob

--- sim/tests/common/riscv_macros.h
+++ sim/tests/common/riscv_macros.h
@@ -9,6 +9,10 @@
 //-----------------------------------------------------------------------
 // Begin Macro
 //-----------------------------------------------------------------------
+#define SC_SIM_OUTPORT (0xf0000000)
+#define RVTEST_PUTCHAR(c)\
+	li a5,(c); \
+	sb a5,0(a2);\
 
 #define RVTEST_RV64U                                                    \
   .macro init;                                                          \
@@ -103,14 +107,24 @@
 
 #define RVTEST_CODE_BEGIN                                               \
         .section .text.init;                                            \
-        .org 0xC0, 0x00;                                                \
+        .org 0x00, 0x00;                                                \
         .align  6;                                                      \
         .weak stvec_handler;                                            \
         .weak mtvec_handler;                                            \
 trap_vector:                                                            \
         /* test whether the test came from pass/fail */                 \
         csrr a4, mcause;                                                \
-        li a5, CAUSE_USER_ECALL;                                        \
+        li a5, CAUSE_BREAKPOINT;					\
+	bne a4,a5,2f;							\
+	li a2, SC_SIM_OUTPORT;						\
+	RVTEST_PUTCHAR('b'); \
+	RVTEST_PUTCHAR('r'); \
+	RVTEST_PUTCHAR('e'); \
+	RVTEST_PUTCHAR('a'); \
+	RVTEST_PUTCHAR('k'); \
+	RVTEST_PUTCHAR('\n'); \
+2:      li a5, CAUSE_USER_ECALL;                                        \
+	li a5, CAUSE_USER_ECALL;                                        \
         beq a4, a5, _report;                                            \
         li a5, CAUSE_SUPERVISOR_ECALL;                                  \
         beq a4, a5, _report;                                            \
@@ -133,6 +147,7 @@ _report:                                                                \
         j sc_exit;                                                      \
         .align  6;                                                      \
         .globl _start;                                                  \
+	.org 0x1a8;\
 _start:                                                                 \
         RISCV_MULTICORE_DISABLE;                                        \
         /*INIT_SPTBR;*/                                                 \
